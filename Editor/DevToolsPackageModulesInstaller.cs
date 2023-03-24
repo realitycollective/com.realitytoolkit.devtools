f2086091ee5c9b901e631f2de20982c794dc90cd
Dino Fejzagić <dino.f@live.de>
2023-03-24 20:13:46 +0100

    Migrate

---
+++ Editor/DevToolsPackageModulesInstaller.cs
@@ -0,0 +1,81 @@
+// Copyright (c) Reality Collective. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using RealityCollective.ServiceFramework.Definitions;
+using RealityCollective.ServiceFramework.Editor.Packages;
+using RealityCollective.ServiceFramework.Services;
+using System.Linq;
+using UnityEditor;
+
+namespace RealityToolkit.DevTools.Editor
+{
+    /// <summary>
+    /// Installs <see cref="IDiagnosticsServiceModule"/>s coming from a third party package
+    /// into the <see cref="DiagnosticsServiceProfile"/> in the <see cref="ServiceManager.ActiveProfile"/>.
+    /// </summary>
+    [InitializeOnLoad]
+    public sealed class DevToolsPackageModulesInstaller : IPackageModulesInstaller
+    {
+        /// <summary>
+        /// Statis initalizer for the installer instance.
+        /// </summary>
+        static DevToolsPackageModulesInstaller()
+        {
+            if (Instance == null)
+            {
+                Instance = new DevToolsPackageModulesInstaller();
+            }
+
+            PackageInstaller.RegisterModulesInstaller(Instance);
+        }
+
+        /// <summary>
+        /// Internal singleton instance of the installer.
+        /// </summary>
+        private static DevToolsPackageModulesInstaller Instance { get; }
+
+        /// <inheritdoc/>
+        public bool Install(ServiceConfiguration serviceConfiguration)
+        {
+            if (!typeof(IDiagnosticsServiceModule).IsAssignableFrom(serviceConfiguration.InstancedType.Type))
+            {
+                // This module installer does not accept the configuration type.
+                return false;
+            }
+
+            if (!ServiceManager.IsActiveAndInitialized)
+            {
+                UnityEngine.Debug.LogWarning($"Could not install {serviceConfiguration.InstancedType.Type.Name}.{nameof(ServiceManager)} is not initialized.");
+                return false;
+            }
+
+            if (!ServiceManager.Instance.HasActiveProfile)
+            {
+                UnityEngine.Debug.LogWarning($"Could not install {serviceConfiguration.InstancedType.Type.Name}.{nameof(ServiceManager)} has no active profile.");
+                return false;
+            }
+
+            if (!ServiceManager.Instance.TryGetServiceProfile<IDiagnosticsService, DiagnosticsServiceProfile>(out var cameraServiceProfile))
+            {
+                UnityEngine.Debug.LogWarning($"Could not install {serviceConfiguration.InstancedType.Type.Name}.{nameof(DiagnosticsServiceProfile)} not found.");
+                return false;
+            }
+
+            // Setup the configuration.
+            var typedServiceConfiguration = new ServiceConfiguration<IDiagnosticsServiceModule>(serviceConfiguration.InstancedType.Type, serviceConfiguration.Name, serviceConfiguration.Priority, serviceConfiguration.RuntimePlatforms, serviceConfiguration.Profile);
+
+            // Make sure it's not already in the target profile.
+            if (cameraServiceProfile.ServiceConfigurations.All(sc => sc.InstancedType.Type != serviceConfiguration.InstancedType.Type))
+            {
+                cameraServiceProfile.AddConfiguration(typedServiceConfiguration);
+                UnityEngine.Debug.Log($"Successfully installed the {serviceConfiguration.InstancedType.Type.Name} to {cameraServiceProfile.name}.");
+            }
+            else
+            {
+                UnityEngine.Debug.Log($"Skipped installing the {serviceConfiguration.InstancedType.Type.Name} to {cameraServiceProfile.name}. Already installed.");
+            }
+
+            return true;
+        }
+    }
+}
